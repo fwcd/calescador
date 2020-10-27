83f13cad6752bcc6c2afbaea307c02225ca2b143
fwcd <fwcdmail@gmail.com>
2020-10-27 23:56:53 +0100

    Present a prettier error message for missing env vars

--- discord/calescador_discord/__main__.py
+++ discord/calescador_discord/__main__.py
@@ -1,10 +1,18 @@
+import sys
 import os
 
 from calescador_discord.bot import create_bot
 
+def getenv(key):
+    value = os.getenv(key)
+    if value == None:
+        print(f'Please make sure that {key} is set!', file=sys.stderr)
+        exit(1)
+    return value
+
 def main():
-    bot = create_bot(command_prefix=';', web_url=os.getenv('CALESCADOR_WEB_URL'))
-    bot.run(os.getenv('CALESCADOR_BOT_TOKEN'))
+    bot = create_bot(command_prefix=';', web_url=getenv('CALESCADOR_WEB_URL'))
+    bot.run(getenv('CALESCADOR_BOT_TOKEN'))
 
 if __name__ == "__main__":
     main()
