8ca5471793473b5af82c9982090b82d2566a3bb1
fwcd <fwcdmail@gmail.com>
2020-10-27 16:53:48 +0100

    Add some more doc comments

--- discord/calescador_discord/api/api_client.py
+++ discord/calescador_discord/api/api_client.py
@@ -10,12 +10,18 @@ class APIClient:
         self.url = url
 
     def create_event(self, event):
+        """Creates a new calendar event on the server."""
+
         requests.post(f'{self.url}/events', data=event.to_dict())
 
     def events(self):
+        """Fetches all calendar events from the server."""
+
         response = requests.get(f'{self.url}/events').json()
         return [Event.from_dict(e) for e in response]
 
     def event(self, id):
+        """Fetches a single event from the server."""
+
         response = requests.get(f'{self.url}/events/{id}').json()
         return Event.from_dict(response)
