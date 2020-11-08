d238fe95415ef78ea25714546b5f00eac10ae50b
fwcd <fwcdmail@gmail.com>
2020-11-08 21:18:42 +0100

    Add calendar component wrapper and add it to home

---
+++ web/resources/js/components/Calendar.vue
@@ -0,0 +1,25 @@
+<!-- Based on sample code from https://fullcalendar.io/docs/vue -->
+
+<template>
+    <FullCalendar :options="calendarOptions" />
+</template>
+
+<script>
+    import FullCalendar from '@fullcalendar/vue';
+    import dayGridPlugin from '@fullcalendar/daygrid';
+    import interactionPlugin from '@fullcalendar/interaction';
+
+    export default {
+        components: {
+            FullCalendar // to make the FullCalendar tag available
+        },
+        data() {
+            return {
+                calendarOptions: {
+                    plugins: [dayGridPlugin, interactionPlugin],
+                    interactionView: 'dayGridMonth'
+                }
+            };
+        }
+    }
+</script>
