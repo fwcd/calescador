a812a41e1e895e4f9ee5fb0a4bdd0681851cd1b7
fwcd <fwcdmail@gmail.com>
2020-11-08 23:24:37 +0100

    Stub out iCalendar (.ics) route

---
+++ web/app/Http/Controllers/ICSController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ICSController extends Controller
+{
+    /** Fetches all calendar events in the iCalendar (.ics) format. */
+    public function all()
+    {
+        //
+    }
+}
