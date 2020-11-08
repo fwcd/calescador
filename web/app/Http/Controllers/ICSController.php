bb56bf515e9941eff8dfa2d37c14e1fd57f3766b
fwcd <fwcdmail@gmail.com>
2020-11-08 23:46:46 +0100

    Add ICS locations

--- web/app/Http/Controllers/ICSController.php
+++ web/app/Http/Controllers/ICSController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Jsvrcek\ICS\Model\Calendar as ICalendar;
 use Jsvrcek\ICS\Model\CalendarEvent as ICalendarEvent;
+use Jsvrcek\ICS\Model\Description\Location as ICalendarLocation;
 use Jsvrcek\ICS\Utility\Formatter as ICalendarFormatter;
 use Jsvrcek\ICS\CalendarStream as ICalendarStream;
 use Jsvrcek\ICS\CalendarExport as ICalendarExport;
@@ -20,14 +21,17 @@ class ICSController extends Controller
         $ical->setProdId('-//calescador//calescador//EN');
 
         foreach (\App\Models\Event::get() as $event) {
-            // TODO: Convert locations
+            $icalLocation = new ICalendarLocation;
+            $icalLocation->setName($event->location);
 
             $icalEvent = new ICalendarEvent;
             $icalEvent
                 ->setSummary($event->name)
                 ->setStart($event->start_dt)
                 ->setEnd($event->end_dt)
-                ->setUid($event->id);
+                ->setUid($event->id)
+                ->addLocation($icalLocation);
+
             $ical->addEvent($icalEvent);
         }
 
